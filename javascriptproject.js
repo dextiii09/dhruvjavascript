e5e327a19c87d629d30a19d9f31b3c2e4e754fb8
dextiii09 <126446072+dextiii09@users.noreply.github.com>
2024-06-02 21:55:56 +0530

    Update javascriptproject.js

--- javascriptproject.js
+++ javascriptproject.js
@@ -1,9 +1,5 @@
-// Create a variable to hold your NFTs
 let nfts = [];
 
-// This function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
 function mintNFT(name, creator, description, dateCreated) {
   const nft = {
     name: name,
@@ -15,8 +11,6 @@ function mintNFT(name, creator, description, dateCreated) {
   nfts.push(nft);
 }
 
-// Create a "loop" that will go through an "array" of NFTs
-// and print their metadata with console.log()
 function listNFTs() {
   nfts.forEach((nft, index) => {
     console.log(`NFT #${index + 1}`);
@@ -28,12 +22,10 @@ function listNFTs() {
   });
 }
 
-// Print the total number of NFTs we have minted to the console
 function getTotalSupply() {
   return nfts.length;
 }
 
-// Call your functions below this line
 mintNFT("NFT 1", "Creator A", "This is the first NFT", "2023-06-01");
 mintNFT("NFT 2", "Creator B", "This is the second NFT", "2023-06-02");
 mintNFT("NFT 3", "Creator C", "This is the third NFT", "2023-06-03");
